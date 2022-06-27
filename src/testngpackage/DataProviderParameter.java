d0bfc0e431094f6e365995c3777f0e5dcfe3f8ca
Gadha Das <gadhada93@gmail.com>
2022-06-27 17:40:25 +0530

    parameter in dataprovide

--- src/testngpackage/DataProviderParameter.java
+++ src/testngpackage/DataProviderParameter.java
@@ -1,5 +1,18 @@
 package testngpackage;
 
-public class DataProviderParameter {
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
+
+
+public class DataProviderParameter {
+		
+		@Parameters({"Uname","pwd"})
+		@Test
+		public void pgm(String Username,String Password)
+		{
+			System.out.println(Username);
+			System.out.println(Password);
+			
+		}
 }
